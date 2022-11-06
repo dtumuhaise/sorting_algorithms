762ec7a7d184ec63c1444c90670de8e7ac856a81
Davis Tumuhaise <tumuhaise@gmail.com>
2022-11-06 20:08:26 +0300

    fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,7 +14,7 @@ void bubble_sort(int *array, size_t size)
     {
 	    return;
     }
-    for (i = 0; i < size - i; i++)
+    for (i = 0; i < size - 1; i++)
     {
 	    for (j = 0; j < size - i - 1; j++)
 	    {
