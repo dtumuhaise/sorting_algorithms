9b16bad3cdc6a5de0a9514a7993fd93ceac1bf31
dtumuhaise <44263038+dtumuhaise@users.noreply.github.com>
2022-11-06 21:03:31 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,23 +8,23 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    	size_t i, j, temp;
+    size_t i, j, temp;
 
-    	if (array == NULL || size < 2)
-    	{
-	    	return;
-    	}
-    	for (i = 0; i < size - 1; i++)
-    	{
-	    	for (j = 0; j < size - i - 1; j++)
-	    	{
-		    	if (array[j] > array[j + 1])
-		    	{
-			    	temp = array[j];
-			    	array[j] = array[j + 1];
-			    	array[j + 1] = temp;
-			    	print_array(array, size);
-		    	}
-	    	}
-    	}
+    if (array == NULL || size < 2)
+    {
+	    return;
+    }
+    for (i = 0; i < size - 1; i++)
+    {
+	    for (j = 0; j < size - i - 1; j++)
+	    {
+		    if (array[j] > array[j + 1])
+		    {
+			    temp = array[j];
+			    array[j] = array[j + 1];
+			    array[j + 1] = temp;
+			    print_array(array, size);
+		    }
+	    }
+    }
 }
