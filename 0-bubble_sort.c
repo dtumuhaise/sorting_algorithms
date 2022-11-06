4f150fc71c3c3f90aaf9148ffb74a94150def9f9
dtumuhaise <tumuhaise@gmail.com>
2022-11-06 08:51:18 -0800

    fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,10 +12,9 @@ void bubble_sort(int *array, size_t size)
 
     if (array == NULL || size < 2)
     {
-        return;
+	    return;
     }
-
-    for (i = 0; i < size - 1; i++)
+    for (i = 0; i < size - i; i++)
     {
 	    for (j = 0; j < size - i - 1; j++)
 	    {
