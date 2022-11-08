97c5a71fc65ffef7ec870ccf3c2c09c33ec01a22
Davis Tumuhaise <tumuhaise@gmail.com>
2022-11-08 13:00:52 +0300

    update insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,82 +1,55 @@
 #include "sort.h"
-
-
-
-listint_t *getLinkNode(const int n)
-{
-	listint_t *list = (listint_t *) malloc(sizeof(listint_t));
-	if (list == NULL)
-	{
-		return (NULL);
-	}
-	list->n = n;
-	list->next = NULL;
-	list->prev = NULL;
-	return (list);
-}
-
-typedef struct DoubleLinkedList
-{
-	struct listint_s *head;
-	struct listint_s *tail;	
-}DoubleLinkedList;
-
-DoubleLinkedList *getDoubleLinkedList()
-{
-	DoubleLinkedList *list = (DoubleLinkedList * ) malloc(sizeof(DoubleLinkedList));
-	if (list == NULL)
-	{
-		return (NULL);
-	}
-	list->head = NULL;
-	list->tail = NULL;
-	return (list);
-}
-
-void insert(DoubleLinkedList *list, int value)
-{
-	listint_t *node = getLinkNode(value);
-	if ((list->head = NULL))
-	{
-		list->head = node;
-		list->tail = node;
-		return;
-	}
-	list->tail->next = node;
-	node->prev = list->tail;
-	list->tail = node;
-}
-
-void swapData(listint_t *first, listint_t *second)
-{
-	int value = first->n;
-	first->n = second->n;
-	second->n = value;
-}
-
 /**
- * selection_sort - sort using insertion sort
- * @list: double linked list to be sorted
- * 
+ * insertion_sort_list - sorts doubly linked list
+ * @list: list to be sorted
 */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *front = *list;
-	listint_t *back = NULL;
+	listint_t *front, *back, *nextnode;
 
-	while (front != NULL)
+	if (list == NULL || !(*list) || (*list)->next == NULL)
 	{
-		back = front->next;
-
-		while (back != NULL &&
-				back->prev != NULL &&
-				back->n < back->prev->n)
-		{
-			swapData(back, back->prev);
-			back = back->prev;			
-		}
-		front = front->next;		
+		return;
 	}
-	
+	front = (*list)->next;
+	nextnode = front->next;
+	while (front)
+	{
+		if (front->n < front->prev->n)
+		{
+			back = front->prev;
+			while (back && (front->n < back->n))
+			{
+				if (!(back->prev))
+				{
+					back->prev = front;
+					front->prev->next = front->next;
+					if (front->next)
+					{
+						front->next->prev = front->prev;
+					}
+					front->next = back;
+					front->prev = NULL;
+					*list = front;
+				}
+				else
+				{
+					front->prev->next = front->next;
+					if (front->next)
+					{
+						front->next->prev = front->prev;
+					}
+					back->prev->next = front;
+					front->prev = back->prev;
+					back->prev = front;
+					front->next = back;
+				}
+				print_list(*list);
+				back = front->prev;
+			}
+		}
+		front = nextnode;
+		front ? (nextnode = front->next) : (nextnode = NULL);
+	}
 }
