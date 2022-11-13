ef6d4fea6cd7692de209cb138792018a7050620f
Davis Tumuhaise <tumuhaise@gmail.com>
2022-11-14 01:34:49 +0300

    update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,9 +11,13 @@ void selection_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
 	for (i = 0; i < size; i++)
 	{
-		for (j = i + 1; j < size; j++)
+		for (j = i; j < size; j++)
 		{
 			if (array[i] > array[j])
 			{
